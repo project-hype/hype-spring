d98433e5942a92fc42b8b01a10d846605ca621c7
eunjeeing <ejjeongggg@gmail.com>
2024-06-17 21:00:52 +0900

    Feat: 이벤트 도메인 개발

--- src/main/java/com/devjeans/hype/event/mapper/EventMapper.java
+++ src/main/java/com/devjeans/hype/event/mapper/EventMapper.java
@@ -3,8 +3,10 @@ package com.devjeans.hype.event.mapper;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import com.devjeans.hype.event.domain.BannerVO;
 import com.devjeans.hype.event.domain.EventVO;
 
 public interface EventMapper {
@@ -16,8 +18,21 @@ public interface EventMapper {
 	@Select("SELECT event_id FROM EVENT")
 	public List<EventVO> getList();
 	
+	// 조회수 순 이벤트 조회 
 	public List<EventVO> getTopViewEvents();
 	
+	// 날짜별 이벤트 조회 
 	public List<EventVO> getEventsByDate(Date date);
 	
+	// 배너 조회 
+	public List<BannerVO> getBannerEvents();
+	
+	// 즐겨찾기 추가 
+	public int insertFavorite(@Param("memberId") Long memberId, @Param("eventId") Long eventId);
+	
+	// 즐겨찾기 제거
+	public int deleteFavorite(@Param("memberId") Long memberId, @Param("eventId") Long eventId);
+
+	// 즐겨찾기 여부 확인
+	public int getCheckFavorite(@Param("memberId") Long memberId, @Param("eventId") Long eventId);
 }
