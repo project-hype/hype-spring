7a6572afcd01fb28e21110e3b0f56d5a2cf291af
YeongWook <ehdrnr16whduddnr@naver.com>
2024-06-17 12:12:57 +0900

    Feat: Category, Hashtag, EventHashtag CRUD 추가

--- src/main/java/com/devjeans/hype/event/mapper/AdminEventMapper.java
+++ src/main/java/com/devjeans/hype/event/mapper/AdminEventMapper.java
@@ -1,6 +1,9 @@
 package com.devjeans.hype.event.mapper;
 
+import com.devjeans.hype.event.domain.CategoryVO;
+import com.devjeans.hype.event.domain.EventHashtagVO;
 import com.devjeans.hype.event.domain.EventVO;
+import com.devjeans.hype.event.domain.HashtagVO;
 
 public interface AdminEventMapper {
 
@@ -9,6 +12,31 @@ public interface AdminEventMapper {
 	public EventVO selectEventById(Long eventId);
 	public int insertEvent(EventVO event);
 	public int updateEvent(EventVO event);
-	public int deleteEvent (Long eventId);	
+	public int deleteEvent (Long eventId);
+	
+	// Category CRUD
+	public CategoryVO selectCategoryById(Long categoryId);
+	public int insertCategory(CategoryVO category);
+	public int updateCategory(CategoryVO category);
+	public int deleteCategory(Long categoryId);
+	
+	// EventType CRUD : 보류
+//	public EventTypeVO selectEventTypeById(Long eventTypeId);
+//	public int insertEventType(EventTypeVO eventType);
+//	public int updateEventType(EventTypeVO eventType);
+//	public int deleteEventType(Long eventTypeId);
+	
+	// Hashtag CRUD
+	public HashtagVO selectHashtagById(Long hashtagId);
+	public int insertHashtag(HashtagVO hashtag);
+	public int updateHashtag(HashtagVO hashtag);
+	public int deleteHashtag(Long hashtagId);
+	
+	// EventHashTag CRUD
+	public EventHashtagVO selectEventHashtagByEventId(Long eventId);
+	public EventHashtagVO selectEventHashtagByHashtagId(Long hashtagId);
+	public int insertEventHashtag(EventHashtagVO eventHashtag);
+//	public int updateEventHashtag(EventHashtagVO eventHashtag); 필요 없음
+	public int deleteEventHashtag(Long eventHashtagId);
 	
 }
--- src/test/java/com/devjeans/hype/persistence/AdminEventMapperTest.java
+++ src/test/java/com/devjeans/hype/persistence/AdminEventMapperTest.java
@@ -13,10 +13,26 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.devjeans.hype.event.domain.CategoryVO;
+import com.devjeans.hype.event.domain.EventHashtagVO;
 import com.devjeans.hype.event.domain.EventVO;
+import com.devjeans.hype.event.domain.HashtagVO;
 import com.devjeans.hype.event.mapper.AdminEventMapper;
 
 import lombok.extern.log4j.Log4j;
+
+/**
+ * 어드민 행사관리 테스트 클래스
+ * @author 조영욱
+ * @since 2024.06.17
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        	수정자        수정내용
+ * ----------  --------    ---------------------------
+ * 2024.06.17  	조영욱        최초 생성
+ * </pre>
+ */
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -27,6 +43,13 @@ public class AdminEventMapperTest {
 	@Autowired
 	private AdminEventMapper mapper;
 	
+	/*****************************
+	 ****** Event CRUD Test ******
+	 *****************************/
+	
+	/**
+	 * Event ID로 조회
+	 */
 	@Test
 	public void testSelectEventById() {
 		EventVO event = mapper.selectEventById(59L);
@@ -36,6 +59,9 @@ public class AdminEventMapperTest {
 		assertEquals(59L, event.getEventId().longValue());
 	}
 	
+	/**
+	 * Event 생성
+	 */
 	@Test
 	public void testInsertEvent() {
 		EventVO event = new EventVO();
@@ -54,6 +80,9 @@ public class AdminEventMapperTest {
 		assertEquals(1, result);
 	}
 	
+	/**
+	 * Event 수정
+	 */
 	@Test
 	public void testUpdateEvent() {
 		EventVO event = mapper.selectEventById(64L);
@@ -83,6 +112,9 @@ public class AdminEventMapperTest {
 		assertEquals(1, result);
 	}
 	
+	/**
+	 * Event 삭제
+	 */
 	@Test
 	public void testDeleteEvent() {
 		int result = mapper.deleteEvent(62L);
@@ -91,4 +123,176 @@ public class AdminEventMapperTest {
 		
 		assertEquals(1, result);
 	}
+	
+	/******************************
+	 ***** Category CRUD Test *****
+	 *****************************/
+	
+	/**
+	 * Category ID로 조회
+	 */
+	@Test
+	public void testSelectCategoryById() {
+		CategoryVO category = mapper.selectCategoryById(1L);
+		
+		log.info(category);
+		
+		assertEquals(1L, category.getCategoryId().longValue());
+	}
+	
+	/**
+	 * Category 생성
+	 */
+	@Test
+	public void testInsertCategory() {
+		CategoryVO category = new CategoryVO();
+		category.setCategoryName("새카테고리");
+		
+		int result = mapper.insertCategory(category);
+		
+		assertEquals(1, result);
+	}
+	
+	/**
+	 * Category 수정
+	 */
+	@Test
+	public void testUpdateCategory() {
+		CategoryVO category = mapper.selectCategoryById(27L);
+		if (category == null) {
+			return;
+		}
+		
+		category.setCategoryName("수정카테고리");
+		
+		int result = mapper.updateCategory(category);
+		
+		assertEquals(1, result);
+	}
+	
+	/**
+	 * Category 삭제
+	 */
+	@Test
+	public void testDeleteCategory() {
+		int result = mapper.deleteCategory(27L);
+		
+		log.info(result);
+		
+		assertEquals(1, result);
+	}
+	
+	/******************************
+	 ****  Hashtag CRUD Test  *****
+	 *****************************/
+	
+	/**
+	 * Hashtag ID로 조회
+	 */
+	@Test
+	public void testSelectHashtagById() {
+		HashtagVO hashtag = mapper.selectHashtagById(1L);
+		
+		log.info(hashtag);
+		
+		assertEquals(1L, hashtag.getHashtagId().longValue());
+	}
+	
+	/**
+	 * Hashtag 생성
+	 */
+	@Test
+	public void testInsertHashtag() {
+		HashtagVO hashtag = new HashtagVO();
+		hashtag.setHashtagName("흰둥이");
+		
+		int result = mapper.insertHashtag(hashtag);
+		
+		assertEquals(1, result);
+	}
+	
+	/**
+	 * Hashtag 수정
+	 */
+	@Test
+	public void testUpdateHashtag() {
+		HashtagVO hashtag = mapper.selectHashtagById(2L);
+		if (hashtag == null) {
+			return;
+		}
+		
+		hashtag.setHashtagName("수정해시태그");
+		
+		int result = mapper.updateHashtag(hashtag);
+		
+		assertEquals(1, result);
+	}
+	
+	/**
+	 * Hashtag 삭제
+	 */
+	@Test
+	public void testDeleteHashtag() {
+		int result = mapper.deleteHashtag(2L);
+		
+		log.info(result);
+		
+		assertEquals(1, result);
+	}
+	
+	/******************************
+	 ****  EventHashtag CRUD Test  *****
+	 *****************************/
+	
+	/**
+	 * EventHashtag Event ID로 조회
+	 */
+	@Test
+	public void testSelectEventHashtagByEventId() {
+		EventHashtagVO eventHashtag = mapper.selectEventHashtagByEventId(1L);
+		
+		log.info(eventHashtag);
+		
+		assertEquals(1L, eventHashtag.getEventId().longValue());
+	}
+	
+	/**
+	 * EventHashtag Hashtag ID로 조회
+	 */
+	@Test
+	public void testSelectEventHashtagByHashtagId() {
+		EventHashtagVO eventHashtag = mapper.selectEventHashtagByHashtagId(1L);
+		
+		log.info(eventHashtag);
+		
+		assertEquals(1L, eventHashtag.getHashtagId().longValue());
+	}
+	
+	/**
+	 * EventHashtag 생성
+	 */
+	@Test
+	public void testInsertEventHashtag() {
+		EventHashtagVO eventHashtag = new EventHashtagVO();
+		eventHashtag.setEventId(1L);
+		eventHashtag.setHashtagId(1L);
+		
+		int result = mapper.insertEventHashtag(eventHashtag);
+		
+		assertEquals(1, result);
+	}
+	
+
+	/**
+	 * EventHashtag 삭제
+	 */
+	@Test
+	public void testDeleteEventHashtag() {
+		int result = mapper.deleteEventHashtag(2L);
+		
+		log.info(result);
+		
+		assertEquals(1, result);
+	}
+	
 }
