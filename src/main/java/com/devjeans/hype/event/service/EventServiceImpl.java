1403e470bd39efcc02d65c3595dae27b9d492da4
eunjeeing <ejjeongggg@gmail.com>
2024-06-17 21:00:52 +0900

    Feat: 이벤트 도메인 개발

--- src/main/java/com/devjeans/hype/event/service/EventServiceImpl.java
+++ src/main/java/com/devjeans/hype/event/service/EventServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.devjeans.hype.event.domain.BannerVO;
 import com.devjeans.hype.event.domain.EventVO;
 import com.devjeans.hype.event.mapper.EventMapper;
 
@@ -20,14 +21,48 @@ public class EventServiceImpl implements EventService {
 
 	@Override
 	public List<EventVO> getListTopView() {
+		
 		log.info("조회수 top10 조회");
 		return mapper.getTopViewEvents();
 	}
 
 	@Override
 	public List<EventVO> getListByDate(Date date) {
-		log.info("날짜별 조회 ");
+		
+		log.info("날짜별 조회");
 		return mapper.getEventsByDate(date);
+	}
+
+	@Override
+	public List<BannerVO> getListBanner() {
+		log.info("배너 조회");
+		return mapper.getBannerEvents();
+	}
+
+	@Override
+	public boolean addFavoriteEvent(Long memberId, Long eventId) {
+	
+		log.info("즐겨찾기 추가");
+		log.info("memberId: " + memberId);
+		log.info("eventId: " + eventId);
+		
+		return mapper.insertFavorite(memberId, eventId) == 1;
+	}
+
+	@Override
+	public boolean deleteFavoriteEvent(Long memberId, Long eventId) {
+		log.info("즐겨찾기 제거");
+		log.info("memberId: " + memberId);
+		log.info("eventId: " + eventId);
+		
+		return mapper.deleteFavorite(memberId, eventId) == 1;
+	}
+
+	@Override
+	public boolean checkFavoriteEvent(Long memberId, Long eventId) {
+		log.info("즐겨찾기 여부 확인");
+		log.info(mapper.getCheckFavorite(memberId, eventId));
+		return mapper.getCheckFavorite(memberId, eventId) == 1;
 	}
 
 }
