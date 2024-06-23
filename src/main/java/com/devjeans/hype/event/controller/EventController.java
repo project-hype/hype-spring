46c0a76e9119cbc1d8ac74e3d1d1886e93b153d4
eunjeeing <ejjeongggg@gmail.com>
2024-06-23 13:56:12 +0900

    Feat: 이벤트 컨트롤러 수정

--- src/main/java/com/devjeans/hype/event/controller/EventController.java
+++ src/main/java/com/devjeans/hype/event/controller/EventController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devjeans.hype.aop.LoginId;
 import com.devjeans.hype.event.domain.BannerVO;
 import com.devjeans.hype.event.domain.EventHashtagVO;
 import com.devjeans.hype.event.domain.EventVO;
@@ -47,11 +46,10 @@ import lombok.extern.log4j.Log4j;
  * <pre>
  * 수정일        	수정자        수정내용
  * ----------  --------    ---------------------------
- * 2024.06.17   정은지        최초 생성
- * 2024.06.20   조영욱        이벤트 필터로 조회 추가
- * 2024.06.21   조영욱        이벤트 검색,필터 조회에 페이지네이션 적용, 카테고리/해시태그 검색 추가
- * 2024.06.22   정은지 
- * 2024.06.21   조영욱        LoginId 어노테이션 적용
+ * 2024.06.17   정은지         최초 생성
+ * 2024.06.20   조영욱         이벤트 필터로 조회 추가
+ * 2024.06.21   조영욱         이벤트 검색,필터 조회에 페이지네이션 적용, 카테고리/해시태그 검색 추가
+ * 2024.06.22   정은지 		  
  * </pre>
  */
 
@@ -140,14 +138,14 @@ public class EventController {
 	@PostMapping("/list/filter")
 	public GetEventListResponse getListWithFilter(
 			@RequestBody @Valid EventFilterRequest request,
-			BindingResult bs, @LoginId Long memberId) throws Exception {
+			BindingResult bs) throws Exception {
 		if (bs.hasErrors()) {
 			log.info(bs);
 			return null;
 		}
 		
 		List<EventVO> list = service.getListWithFilter(request);
-		List<Long> favoriteEventIds = service.getMyFavoriteEvent(memberId);
+		List<Long> favoriteEventIds = service.getMyFavoriteEvent(null);
 		boolean isNextEventExist = service.isNextEventExist(request);
 		return new GetEventListResponse(list, favoriteEventIds, isNextEventExist);
 	}
